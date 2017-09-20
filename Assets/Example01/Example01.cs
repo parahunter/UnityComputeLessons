e05efb537a418b5905a0edb905cb7352ded43ef7
Alexander Birke <parahunter@gmail.com>
2017-09-20 17:15:48 +0100

    first example

---
+++ Assets/Example01/Example01.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example01 : MonoBehaviour
+{
+	[SerializeField] ComputeShader computeShader;
+
+	RenderTexture texture;
+
+	const int textureSize = 512;
+	const int kernelSize = 8;
+
+	// Use this for initialization
+	void Start ()
+	{
+		texture = new RenderTexture(width: textureSize, height: textureSize, depth: 0);
+		texture.enableRandomWrite = true;
+		texture.filterMode = FilterMode.Point;
+		texture.Create();
+
+		int kernelIndex = computeShader.FindKernel("CSMain");
+
+		computeShader.SetTexture(kernelIndex: kernelIndex, name: "Result", texture: texture);
+
+		computeShader.Dispatch(kernelIndex, textureSize / kernelSize, textureSize / kernelSize, 1);
+
+		GetComponent<Renderer>().material.mainTexture = texture;
+    }
+
+	void OnDisable()
+	{
+		if (texture != null)
+			texture.Release();
+	}
+}
