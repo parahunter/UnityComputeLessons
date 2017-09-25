8587ff5d3efc156b4e21ba98a8f329ce0fc266ab
Alexander Birke <parahunter@gmail.com>
2017-09-25 14:38:54 +0100

    noclip camera

--- Assets/Generic/NoclipCamera.cs
+++ Assets/Generic/NoclipCamera.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NoclipCamera : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+public class NoclipCamera : MonoBehaviour
+{
+	const float moveSpeed = 10f;
+	const float rotationSpeed = 200f;
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
+		Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		Vector2 rotationInput = new Vector2(Input.GetAxis("Mouse X"), -Input.GetAxis("Mouse Y"));
+
+		Vector3 moveDelta = transform.forward * moveInput.y + transform.right * moveInput.x;
+		moveDelta *= moveSpeed * Time.deltaTime;
+		transform.Translate(moveDelta);
+
+		if (Input.GetMouseButton(0))
+		{
+			transform.Rotate(Vector3.up * rotationInput.x * rotationSpeed * Time.deltaTime);
+			transform.Rotate(transform.right, rotationInput.y * rotationSpeed * Time.deltaTime);
+			Vector3 eulers = transform.eulerAngles;
+			eulers.z = 0;
+			transform.eulerAngles = eulers;
+		}
 	}
 }
