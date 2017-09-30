577daffd160eaf7a77aca067e5e4ca50f395104d
Alexander Birke <parahunter@gmail.com>
2017-09-30 16:46:18 +0100

    LDS example work

---
+++ Assets/Example04/Example04.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example04 : MonoBehaviour
+{
+	public ComputeShader shader;
+
+	RenderTexture outputTex;
+
+	const int blockWidth = 40;
+	const int blockHeight = 25;
+
+	const int cellWidth = 8;
+	const int cellHeight = 8;
+
+	public int mode = 0;
+	const int num_stages = 3;
+	string[] kernalStage = new string[num_stages]{
+		"CSHiRes_OnlyRes",
+		"CSHiRes_ResCol",
+		"CSHiRes"
+	};
+
+	bool hasCreatedResources = false;
+
+	// Use this for initialization
+	void CreateResources()
+	{
+		outputTex = new RenderTexture(blockWidth * cellWidth, blockHeight * cellHeight, 0);
+		outputTex.enableRandomWrite = true;
+		outputTex.filterMode = FilterMode.Point;
+
+		outputTex.Create();
+		hasCreatedResources = true;
+	}
+	
+	void OnDestroy()
+	{
+		outputTex.Release();
+		hasCreatedResources = false;
+	}
+
+	private void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		if (!hasCreatedResources)
+			CreateResources();
+
+		int kernelHandle = shader.FindKernel(kernalStage[mode]);
+		shader.SetTexture(kernelHandle, "SourceTex", source);
+		shader.SetTexture(kernelHandle, "Result", outputTex);
+		shader.Dispatch(kernelHandle, blockWidth, blockHeight, 1);
+
+		//A bit of a waste but only way Unity allows us to 
+		Graphics.Blit(outputTex, destination);
+	}
+	
+}
